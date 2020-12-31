2d8580ffbbaf68a7761b02414ba2f5ffbfb82127
asthachauhan55 <75516077+asthachauhan55@users.noreply.github.com>
2020-12-31 23:43:24 +0530

    Add files via upload

---
+++ if2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+        printf("Enter 3 numbers-");
+        scanf("%d %d %d",&a,&b,&c);
+    if((a>b)&&(a>c))
+    
+        
+        printf("A is GREATER");
+
+    
+    if((b>a)&&(b>c))
+    
+        printf("B is GREATER");
+
+    if((c>a)&&(c>b))
+    
+        printf("C is GREATER");
+    
+    
+    return 0;
+}   
+    
