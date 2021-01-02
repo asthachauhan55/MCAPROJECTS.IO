887ddcb89cf7693725e29174a0e950b4f9654cd4
asthachauhan55 <75516077+asthachauhan55@users.noreply.github.com>
2021-01-02 18:52:02 +0530

    Add files via upload

---
+++ prg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+     
+    printf("enter a number a=");
+    scanf("%d",&a);
+ if (a%2==0)
+    printf("a is even");
+ else
+    printf("a is odd");
+    return 0;
+    getch();
+}
+
+
+
+    
+
+
