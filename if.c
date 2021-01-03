27c54b6b560c8e77196eeea8e10c2268ad7b4e2b
asthachauhan55 <75516077+asthachauhan55@users.noreply.github.com>
2021-01-03 23:56:46 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("Enter Age-");
+    scanf("%d",&a);
+    if (a>18)
+    {
+        printf("Age is MAJOR");
+    }
+    else
+    {
+        printf("Age is MINOR");
+    
+    }
+    
+return 0;
+getch();
+}
